1b7eeeb7621cc0ffd04b2113e9c14b00ca2f2449
Maria <honey.shamonova@lis.ru>
2020-03-31 21:08:14 +0300

    Second version

---
+++ php/header.php
@@ -0,0 +1,9 @@
+<div class="top-menu container header" id="header">
+    <div class="menu-items row ">
+        <div class="col-md-1 col-md-offset-1  logo-item"><p>ST</p></div>
+        <div class="col-md-1 catalog-item"><p>Каталог</p></div>
+        <div class="col-md-1 basket-item"><p>Корзина</p></div>
+        <div class="col-md-1 contacts-item"><p>Контакты</p></div>
+        <div class="col-md-1 enter-item"><p>Войти</p></div>
+    </div>
+</div>
